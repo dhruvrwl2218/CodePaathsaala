34da14e9b96826a6046259c7ace4f583323e463f
dhruvrwl2218 <dhruvrwl2218@gmail.com>
2024-08-29 14:35:56 +0530

    removed razorpay gateway

--- Client/src/utils/AxiosInstance.jsx
+++ Client/src/utils/AxiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/api/v1/',
+    baseURL: 'https://codepaathsaala-3.onrender.com/api/v1/',
     headers:{
         'Content-Type':'application/json'
     },
